b812b617cb234baa1cb8c9d9bc6e3e307cc86ce3
Miguel Acevedo <miguel.acevedo@exaleap.ai>
2021-11-09 18:26:37 +0800

    fix: responsive

--- components/resume/index.tsx
+++ components/resume/index.tsx
@@ -19,21 +19,21 @@ const Resume = () => {
 
     return (
         <>
-            <div className="grid grid-cols-8">
-                <div className="col-span-2 resume-bg">
+            <div className="lg:grid grid-cols-8">
+                <div className="hidden lg:inline col-span-2 resume-bg">
                     <div>
                         <img className="mx-auto rounded-full mt-16 mb-16" alt="profile picture" src={cv.basics.image} />
                     </div>
                 </div>
                 <div className="col-span-6">
                     <div className="p-6 pt-16">
-                        <div className="text-6xl uppercase resume-color-2 antialiased font-bold">{cv.basics.name}</div>
-                        <div className="text-2xl uppercase mt-1 resume-color-4 antialiased font-semibold">{cv.basics.label}</div>
+                        <div className="text-3xl lg:text-6xl uppercase resume-color-2 antialiased font-bold">{cv.basics.name}</div>
+                        <div className="lg:text-2xl uppercase mt-1 resume-color-4 antialiased font-semibold">{cv.basics.label}</div>
                         <div className="mt-6 resume-color-5">{cv.basics.summary}</div>
                     </div>
                 </div>
             </div>
-            <div className="grid grid-cols-8 ">
+            <div className="lg:grid grid-cols-8 ">
                 <div className="col-span-2 resume-bg text-gray-300">
                     <div className="text-center">
                         <a className="text-gray-100 bg-yellow-600 rounded-full py-4 px-8" href={nextConfig.resumePath}>Download CV</a>
