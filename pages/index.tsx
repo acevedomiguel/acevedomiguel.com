109e25f7158376af00989a31b61ee3bfcb9079ee
acevedomiguel <acevedomiguel@gmail.com>
2023-05-22 13:15:56 +0800

    fix: resume dates and bio

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,9 @@ export default function Index() {
                         <div className='border-t border-gray-200 '>
                             <div className="p-6 items-center justify-center">
                                 <div className='mb-6 text-sm'>
-                                <p>Target-oriented, senior software developer, with extensive experience (over 20 years) in the digital marketing world and IoT. Knowledge of web technologies and understanding of devops and infrastructure.</p>
+                                <p className='mb-3' >Hi, I'm Miguel Angel Acevedo, a <strong>DevOps and Backend Engineer</strong> with over 20 years of coding experience.</p>
+                                <p className='mb-3'>I started as an intern in high school, and since then, I have been exploring different languages and paradigms in this never-stopping industry. I have worked in <strong>marketing, social media, advertising, IoT, and cloud computing</strong> for various clients and projects across Latin America and Asia. </p>
+                                <p>I'm currently the <strong>Cloud Lead & IoT</strong> at <strong>Tensor Energy</strong>, a company that provides smart energy solutions. I enjoy learning from other developers, keeping up with the latest technologies, and making toys and furniture for my family in my spare time. I'm also interested in the growing market of space and the opportunities it offers for infrastructure, communication, embedded system, security, and reliability.</p>
                                 </div>
                                 <div className='justify-center flex pt-2'>
                                     <Link href="/resume" passHref={true} >
