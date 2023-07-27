8c059895015df40b7efaeacf178d60d4d88aa6cd
Miguel <acevedomiguel@gmail.com>
2023-07-27 10:55:27 +0800

    updated ga tag

--- components/ga/index.tsx
+++ components/ga/index.tsx
@@ -9,17 +9,13 @@ export const GA = ({ code = "" }) => {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${code}`}
         />
-        <script
-            dangerouslySetInnerHTML={{
-                __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${code}', {
-                page_path: window.location.pathname,
-                });`,
-            }}
-        />
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${code}');
+        </script>
     </>)
 }
 
