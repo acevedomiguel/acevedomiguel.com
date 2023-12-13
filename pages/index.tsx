ee7f199457dec08ab8a54c732117fb11be28a3b7
Miguel Angel Acevedo <acevedomiguel@gmail.com>
2023-12-13 22:15:29 +0800

    fix name

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Index() {
                         <div className=''>
                             <div className="p-6 items-center justify-center">
                                 <div className='mb-6'>
-                                <p className='mb-3' >Hi, I&apos;m Miguel Angel Acevedo, a <strong>DevOps and Backend Engineer</strong> with over 20 years of coding experience.</p>
+                                <p className='mb-3' >Hi, I&apos;m Acevedo Miguel, a <strong>DevOps and Backend Engineer</strong> with over 20 years of coding experience.</p>
                                 <p className='mb-3'>I started as an intern in high school, and since then, I have been exploring different languages and paradigms in this never-stopping industry. I have worked in <strong>marketing, social media, advertising, IoT, and cloud computing</strong> for various clients and projects across Latin America and Asia. </p>
                                 <p>I&apos;m currently the <strong>Cloud Lead & IoT</strong> at <strong>Tensor Energy</strong>, a company that provides smart energy solutions. I enjoy learning from other developers, keeping up with the latest technologies, and making toys and furniture for my family in my spare time. I&apos;m also interested in the growing market of space and the opportunities it offers for infrastructure, communication, embedded system, security, and reliability.</p>
                                 </div>
